42c82c77684d1e666ec65e4a0f1c666ae264c99c
ParadiseDevil <93950967+ParadiseDevil@users.noreply.github.com>
2026-05-02 20:40:34 +0300

    Initial Commit

---
+++ CoordinatorOnePath/CoordinatorOnePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorOnePathApp.swift
+//  CoordinatorOnePath
+//
+//  Created by Дамир Шаймуханбетов on 02.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorOnePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
